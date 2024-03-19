c1ad6dbffe074c137027be9eb41a822023b0b2e2
lsy <shaoyang_lian@uyoung.com.cn>
2024-03-19 09:00:15 +0800

    fix

--- utils/common.py
+++ utils/common.py
@@ -7,6 +7,7 @@ def check_cloud(name, local_ver=1.0):
     res = requests.get("https://jihulab.com/vhook/control/-/raw/main/ql.json")
     if res.status_code == 200:
         verRes = res.json().get(name, {})
+        print(verRes)
         if verRes:
             r_ver = verRes['ver']
             r_log = verRes['log']
@@ -15,6 +16,10 @@ def check_cloud(name, local_ver=1.0):
             r_date = verRes['date']
             r_tg = verRes['tg']
             r_inviteUrl =  verRes['inviteUrl']
+            if not r_open:
+                content = r_tip
+                content += f"【反馈群】：{r_tg}"
+                exit(content)
             if local_ver < r_ver:
                 content = "发现新版本:"
                 content += f"\n【反馈群】：{r_tg}"
@@ -24,10 +29,7 @@ def check_cloud(name, local_ver=1.0):
                 if r_inviteUrl:
                     content += f"\n【项目入口】：{r_inviteUrl}"
                 exit(content)
-            if not r_open:
-                content = r_tip
-                content += f"【反馈群】：{r_tg}"
-                exit(content)
+
             content = f"\n【反馈群】：{r_tg}"
             content += f"\n【当前版本】：V{local_ver}"
             content += f"\n【更新日期】：{r_date}"
